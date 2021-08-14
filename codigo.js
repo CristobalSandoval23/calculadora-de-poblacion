500f39726be9868e37a2393b69aa49ab325de88d
Cristóbal Sandoval <61505785+CristobalSandoval23@users.noreply.github.com>
2021-08-14 02:02:09 -0400

    first commit

---
+++ codigo.js
@@ -0,0 +1,37 @@
+const d = document,
+      $btn =d.querySelectorAll(".btn"),
+      $text =d.getElementById("text");
+
+    $btn[5].style.width = "100px";
+    $btn[5].style.background = "white";
+    $btn[5].style.color = "black";
+
+    function Ecuacion(muestra,confianza, exito, fracaso, error){
+        resolveConfianza = Math.pow(confianza,2);
+        resolveError = Math.pow(error,2);
+        resolve = Math.floor((((resolveConfianza*(exito*fracaso))*muestra))/((resolveError*(muestra-1))+(resolveConfianza*(exito*fracaso))))
+        console.log(resolve);
+    }
+
+
+    $btn[5].addEventListener("click",()=>{
+
+        var muestra = $btn[0].value;
+        var confianza = $btn[1].value;
+        var exito = $btn[2].value;
+        var fracaso =$btn[3].value;
+        var error = $btn[4].value;
+
+        resultado = Ecuacion(muestra, confianza, exito, fracaso, error)
+        if (resultado === undefined){
+            resultado = "Coloca bien los Nº o llena todas las casillas"
+        }
+        $text.textContent = `${resultado}`;
+
+        console.log(resultado )
+    }
+    )
+
+
+
+
